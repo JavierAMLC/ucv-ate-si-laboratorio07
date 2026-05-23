f61cf5bf5e3759b2a6e87b1275737dcc291fef2d
Anderson Javier Musaja Lopez <musajalopez81@gmail.com>
2026-05-22 22:20:20 -0500

    CI

--- si-image-processing-lab/si-image-processing-lab/tests/test_image_processing.py
+++ si-image-processing-lab/si-image-processing-lab/tests/test_image_processing.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
 from si_image_processing_lab.pipelines.image_processing.nodes import process_image
 
 def test_process_image():
@@ -9,5 +13,4 @@ def test_process_image():
         "data/03_primary/test_output.jpg"
     )
     
-    # Valida que la imagen procesada se haya guardado con éxito
     assert os.path.exists("data/03_primary/test_output.jpg")
