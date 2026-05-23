1199c1965c130771e108df36de6d01d8a42698c5
Anderson Javier Musaja Lopez <musajalopez81@gmail.com>
2026-05-22 22:18:06 -0500

    CI

--- si-image-processing-lab/si-image-processing-lab/tests/test_image_processing.py
+++ si-image-processing-lab/si-image-processing-lab/tests/test_image_processing.py
@@ -1,18 +1,13 @@
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
-
-from si_image_processing_lab.pipelines.image_processing.nodes import (
-    process_image,
-)
-
+import os
+from si_image_processing_lab.pipelines.image_processing.nodes import process_image
 
 def test_process_image():
-    result = process_image(
+    os.makedirs("data/03_primary", exist_ok=True)
+    
+    process_image(
         "data/01_raw/marte.jpg",
         "data/03_primary/test_output.jpg"
     )
-
-    assert result is not None
     
+    # Valida que la imagen procesada se haya guardado con éxito
+    assert os.path.exists("data/03_primary/test_output.jpg")
